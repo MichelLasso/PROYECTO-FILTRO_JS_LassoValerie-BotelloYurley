7e90520592ac337cfa16225ac045d07ab8354d5a
yurleyBG <yurleybgarcia20@gmail.com>
2024-09-19 12:56:38 -0500

    feat :sparkles: rocket page fixes and redirects

--- SPACE-X/Rockets/js/script.js
+++ SPACE-X/Rockets/js/script.js
@@ -1,48 +1,41 @@
 document.getElementById("pasar-Pagina").addEventListener("click", fun11)
-
 function fun11(event){
-    var link= document.getElementsByClassName("link");
-    let valu=1;
-    activelink(event)
-    function activelink(event){
-        for( let i of link){
-            i.classList.remove("active");
-        }
-        event.target.classList.add("active");
-        valu=event.target.value;
-        console.log(valu)
-        
-    }
-    function btnprev(){
-        if(valu>0){
-            for( let i of link){
-                i.classList.remove("active");
-            }
-            link[valu-1].classList.add("active");
-        }
-    }
-    btnprev()
-    function btnnext(){
-        if(valu<4){
-            for( let i of link){
-                i.classList.remove("active");
-            }
-            link[valu+1].classList.add("active");
-        }
-    }
-
-    btnnext()
-  
     fetch("https://api.spacexdata.com/v4/rockets")
     .then(res=>res.json())
     .then(data=>{
-       document.getElementById("titulo").innerHTML=`
-            ${data[0].name}
-       `
-       
+        var link= document.getElementsByClassName("link");
+        let valu=document.getElementById("5e9d0d95eda69955f709d1eb");
+        let valu1=document.getElementById("5e9d0d95eda69973a809d1ec");
+        activelink(event)
+        function activelink(event){
+            for( let i of link){
+                i.classList.remove("active");
+            }
+            event.target.classList.add("active");
+        }
+        
+        if(valu.id===data[0].id){
+            console.log(data[0].name)
+            document.getElementById("titulo").innerHTML=`
+                ${data[0].name}
+            `
+        }
+        if(valu1.id===data[1].id){
+            console.log(valu1.id);
+            console.log(data[1].name)
+            document.getElementById("titulo").innerHTML=`
+                ${data[1].name}
+            `
+        
+        }
             
         
     })
+       
+    
+  
+  
 }
+
 
 

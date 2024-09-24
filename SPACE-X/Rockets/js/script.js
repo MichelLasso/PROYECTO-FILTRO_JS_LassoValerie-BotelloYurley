27069eb26b4aafb77cc7189bb7eaa189296ae3b0
yurleyBG <yurleybgarcia20@gmail.com>
2024-09-23 23:18:03 -0500

    feat :sparkles: developments and fix de circle progress in pagerockets

--- SPACE-X/Rockets/js/script.js
+++ SPACE-X/Rockets/js/script.js
@@ -27,21 +27,22 @@ function mostrar(){
             <h4><span  id="span">Number</span><br>${data[0].engines.number} </h4>
         `
         document.getElementById("Info-I").innerHTML=`
-            <p id="info2">Height</p>
+            <p id="info2">Height--22.25mts--73ft</p>
             <progress  value="${data[0].height.meters}" max="118"></progress>
-            <p id="info2">Diameter</p>
+            <p id="info2">Diameter--1.68mts--5.5ft</p>
             <progress value="${data[0].diameter.meters}" max="13"></progress>
-            <p id="info2">Mass</p>
+            <p id="info2">Mass--30146kg--66460lb</p>
             <progress value="${data[0].mass.kg}" max="1420788"></progress>
-            <p id="info2">Thrust Sea Level</p>
+            <p id="info2">Thrust Sea Level--420KN--94000lbf</p>
             <progress value="${data[0].first_stage.thrust_sea_level.lbf}" max="128000"></progress>
-            <p id="info2">Thrust Vacuum</p>
+            <p id="info2">Thrust Vacuum--480KN--110000lbf</p>
             <progress value="${data[0].first_stage.thrust_vacuum.lbf}" max="138000"></progress>
-            <p id="info2">Fuel Amount Tons</p>
+            <p id="info2">Fuel Amount Tons--44.3</p>
             <progress value="${data[0].first_stage.fuel_amount_tons}" max="3300"></progress>
-            <p id="info2">Thrust</p>
+            <p id="info2">Thrust--31KN--7000lbf</p>
             <progress value="${data[0].second_stage.thrust.kN}" max="1957"></progress>
         `
+        progress()
       
     })
 }
@@ -64,6 +65,7 @@ function fun11(event){
             valor=event.target.value;
             if(valor==1){
                 mostrar()
+             
             }
             if(valor==2){
                 console.log(data[1].name)
@@ -94,21 +96,22 @@ function fun11(event){
                     <h4><span  id="span">Number</span><br>${data[1].engines.number} </h4>
                 `
                 document.getElementById("Info-I").innerHTML=`
-                    <p id="info2">Height</p>
+                    <p id="info2">Height--70mts--229.6ft</p>
                     <progress  value="${data[1].height.meters}" max="118"></progress>
-                    <p id="info2">Diameter</p>
+                    <p id="info2">Diameter--3.7mts--12ft</p>
                     <progress value="${data[1].diameter.meters}" max="13"></progress>
-                    <p id="info2">Mass</p>
+                    <p id="info2">Mass--549054kg--1207920lb</p>
                     <progress value="${data[1].mass.kg}" max="1420788"></progress>
-                    <p id="info2">Thrust Sea Level</p>
+                    <p id="info2">Thrust Sea Level--7607KN--1710000lbf</p>
                     <progress value="${data[1].first_stage.thrust_sea_level.lbf}" max="128000"></progress>
-                    <p id="info2">Thrust Vacuum</p>
+                    <p id="info2">Thrust Vacuum--8227KN--1849500lbf</p>
                     <progress value="${data[1].first_stage.thrust_vacuum.lbf}" max="138000"></progress>
-                    <p id="info2">Fuel Amount Tons</p>
+                    <p id="info2">Fuel Amount Tons--385</p>
                     <progress value="${data[1].first_stage.fuel_amount_tons}" max="3300"></progress>
-                    <p id="info2">Thrust</p>
+                    <p id="info2">Thrust--934KN--210000lbf</p>
                     <progress value="${data[1].second_stage.thrust.kN}" max="1957"></progress>
                 `
+                progreess1()
             }
             if(valor==3){
                 console.log(data[2].name)
@@ -139,21 +142,23 @@ function fun11(event){
                     <h4><span id="span">Number</span><br>${data[2].engines.number} </h4>
                 `
                 document.getElementById("Info-I").innerHTML=`
-                    <p id="info2">Height</p>
+                    <p id="info2">Height--70mt--229.6ft</p>
                     <progress  value="${data[2].height.meters}" max="118"></progress>
-                    <p id="info2">Diameter</p>
+                    <p id="info2">Diameter--12.2mt--39.9ft</p>
                     <progress value="${data[2].diameter.meters}" max="13"></progress>
-                    <p id="info2">Mass</p>
+                    <p id="info2">Mass--1420788kg--3125735lb</p>
                     <progress value="${data[2].mass.kg}" max="1420788"></progress>
-                    <p id="info2">Thrust Sea Level</p>
+                    <p id="info2">Thrust Sea Level--246819KN--5130000lbf</p>
                     <progress value="${data[2].first_stage.thrust_sea_level.lbf}" max="128000"></progress>
-                    <p id="info2">Thrust Vacuum</p>
+                    <p id="info2">Thrust Vacuum--24681KN--5548500lbf</p>
                     <progress value="${data[2].first_stage.thrust_vacuum.lbf}" max="138000"></progress>
-                    <p id="info2">Fuel Amount Tons</p>
+                    <p id="info2">Fuel Amount Tons--1155</p>
                     <progress value="${data[2].first_stage.fuel_amount_tons}" max="3300"></progress>
-                    <p id="info2">Thrust</p>
+                    <p id="info2">Thrust--934KN--210000lbf</p>
                     <progress value="${data[2].second_stage.thrust.kN}" max="1957"></progress>
                 `
+                progreess2()
+              
             }
             if(valor==4){
                 console.log(data[3].name)
@@ -171,7 +176,7 @@ function fun11(event){
                 esto.forEach(element => {
                     console.log(element)
                     document.getElementById("imagen").innerHTML=`
-                        <img id="fotosnaves" src="${element}" alt="img">
+                        <img id="fotosnaves" src="https://live.staticflickr.com/65535/48953946911_e60c5bcc5c_b.jpg" alt="img">
                     `
                 
                 })
@@ -184,40 +189,23 @@ function fun11(event){
                     <h4><span id="span">Number</span><br>${data[3].engines.number} </h4>
                 `
                 document.getElementById("Info-I").innerHTML=`
-                    <p id="info2">Height</p>
+                    <p id="info2">Height--118mt--387ft</p>
                     <progress  value="${data[3].height.meters}" max="118"></progress>
-                    <p id="info2">Diameter</p>
+                    <p id="info2">Diameter--9mt--30ft</p>
                     <progress value="${data[3].diameter.meters}" max="13"></progress>
-                    <p id="info2">Mass</p>
+                    <p id="info2">Mass--1335000kg--2943000lb</p>
                     <progress value="${data[3].mass.kg}" max="1420788"></progress>
-                    <p id="info2">Thrust Sea Level</p>
+                    <p id="info2">Thrust Sea Level--128000KN--28775544lbf</p>
                     <progress value="${data[3].first_stage.thrust_sea_level.lbf}" max="128000"></progress>
-                    <p id="info2">Thrust Vacuum</p>
+                    <p id="info2">Thrust Vacuum--138000KN--31023634lbf</p>
                     <progress value="${data[3].first_stage.thrust_vacuum.lbf}" max="138000"></progress>
-                    <p id="info2">Fuel Amount Tons</p>
-                    <progress value="${data[3].first_stage.fuel_amount_tons}" max="3300"></progress>
-                    <p id="info2">Thrust</p>
+                    <p id="info2">Fuel Amount Tons--3300</p>
+                    <progress value="${data[3].first_stage.fuel_amount_tons}" max="3300">height</progress>
+                    <p id="info2">Thrust--1957KN--440000lbf</p>
                     <progress value="${data[3].second_stage.thrust.kN}" max="1957"></progress>
                 `
-                const progressc11=document.getElementsByClassName('progressc1')[0];
-                const blocked=document.getElementsByClassName('block');
-
-                for(var i = 1; i < 100; i ++){
-                    progressc11.innerHTML+=`<div class="block"></div>`
-                    blocked[i].style.transform="rotate("+3.6 * i +"deg)"
-                    blocked[i].style.animationDelay=`${i/45}s`;
-
-                }
-                const circulo=document.querySelector('.circulo')
-                let target = +circulo.getAttribute('data-target');
-                target=data[3].second_stage.thrust.kN;
-                for (let a = 0; a < target; a++) {
-                    blocked[a].classList.add('target')
-                    
-                }
-                console.log(circulo);
-                console.log(target);
-                console.log(blocked);
+                progreess3()
+            
 
             
             }
@@ -226,7 +214,7 @@ function fun11(event){
     })
  
 }
-function progreess(){
+function progress(){
     fetch("https://api.spacexdata.com/v4/rockets")
     .then(res=>res.json())
     .then(data=>{
@@ -241,40 +229,231 @@ function progreess(){
         }
         const circulo=document.querySelector('.circulo')
         let target = +circulo.getAttribute('data-target');
-        target=data[0].second_stage.payloads.composite_fairing.height.meters;
+        let totalPH= data[0].second_stage.payloads.composite_fairing.height.meters/100*36.0
+        target=totalPH;
         for (let a = 0; a < target; a++) {
             blocked[a].classList.add('target')
             
         }
-        
-        
-        
-        
-        
-       
+        let progressc22=document.getElementsByClassName('progressc2')[0];
+        let block22=document.getElementsByClassName('block2');
     
+        for(var r = 1; r < 100; r ++){
+            progressc22.innerHTML+=`<div class="block2"></div>`
+            block22[r].style.transform="rotate("+3.6 * r +"deg)"
+            block22[r].style.animationDelay=`${r/45}s`;
+    
+        }
+        const circulo2=document.querySelector('.circulo2')
+        let target2 = +circulo2.getAttribute('data-target');
+        let totalPD= data[0].second_stage.payloads.composite_fairing.diameter.meters/100*36.0
+        target2=totalPD;
+        for (let l = 0; l < target2; l++) {
+            block22[l].classList.add('target2')
+            
+        }
+        let progressc33=document.getElementsByClassName('progressc3')[0];
+        let block33=document.getElementsByClassName('block3');
+    
+        for(var b = 1; b < 100; b ++){
+            progressc33.innerHTML+=`<div class="block3"></div>`
+            block33[b].style.transform="rotate("+3.6 * b+"deg)"
+            block33[b].style.animationDelay=`${b/45}s`;
+    
+        }
+        const circulo3=document.querySelector('.circulo3')
+        let target3 = +circulo3.getAttribute('data-target');
+        let totalTw= data[0].engines.thrust_to_weight/100*36.0
+        target3=totalTw;
+        for (let c = 0; c < target3; c ++) {
+            block33[c].classList.add('target3')
+            
+        }
+        console.log(target3)
     })
-   
+  
+}
+
+function progreess1(){
+    fetch("https://api.spacexdata.com/v4/rockets")
+    .then(res=>res.json())
+    .then(data=>{
+        let progressc11=document.getElementsByClassName('progressc1')[0];
+        let blocked=document.getElementsByClassName('block');
+    
+        for(var i = 1; i < 100; i ++){
+            progressc11.innerHTML+=`<div class="block"></div>`
+            blocked[i].style.transform="rotate("+3.6 * i +"deg)"
+            blocked[i].style.animationDelay=`${i/45}s`;
+    
+        }
+        const circulo=document.querySelector('.circulo')
+        let target = +circulo.getAttribute('data-target');
+        let totalPH= data[1].second_stage.payloads.composite_fairing.height.meters/100*36.0
+        target=totalPH;
+        for (let a = 0; a < target; a++) {
+            blocked[a].classList.add('target')
+            
+        }
+        console.log(target)
+        let progressc22=document.getElementsByClassName('progressc2')[0];
+        let block22=document.getElementsByClassName('block2');
+    
+        for(var i = 1; i < 100; i ++){
+            progressc22.innerHTML+=`<div class="block2"></div>`
+            block22[i].style.transform="rotate("+3.6 * i +"deg)"
+            block22[i].style.animationDelay=`${i/45}s`;
+    
+        }
+        const circulo2=document.querySelector('.circulo2')
+        let target2 = +circulo2.getAttribute('data-target');
+        let totalPD= data[1].second_stage.payloads.composite_fairing.diameter.meters/100*36.0
+        target2=totalPD;
+        for (let a = 0; a < target2; a++) {
+            block22[a].classList.add('target2')
+            
+        }
+        console.log(target2)
+        let progressc33=document.getElementsByClassName('progressc3')[0];
+        let block33=document.getElementsByClassName('block3');
+    
+        for(var i = 1; i < 100; i ++){
+            progressc33.innerHTML+=`<div class="block3"></div>`
+            block33[i].style.transform="rotate("+3.6 * i +"deg)"
+            block33[i].style.animationDelay=`${i/45}s`;
+    
+        }
+        const circulo3=document.querySelector('.circulo3')
+        let target3 = +circulo3.getAttribute('data-target');
+        let totalTw= data[1].engines.thrust_to_weight/100*36.0
+        target3=totalTw;
+        for (let a = 0; a < target3; a++) {
+            block33[a].classList.add('target3')
+            
+        }
+        console.log(target3)
+    })
+}
+
+function progreess2(){
+    fetch("https://api.spacexdata.com/v4/rockets")
+    .then(res=>res.json())
+    .then(data=>{
+        let progressc11=document.getElementsByClassName('progressc1')[0];
+        let blocked=document.getElementsByClassName('block');
+    
+        for(var i = 1; i < 100; i ++){
+            progressc11.innerHTML+=`<div class="block"></div>`
+            blocked[i].style.transform="rotate("+3.6 * i +"deg)"
+            blocked[i].style.animationDelay=`${i/45}s`;
+    
+        }
+        const circulo=document.querySelector('.circulo')
+        let target = +circulo.getAttribute('data-target');
+        let totalPH= data[2].second_stage.payloads.composite_fairing.height.meters/100*36.0
+        target=totalPH;
+        for (let a = 0; a < target; a++) {
+            blocked[a].classList.add('target')
+            
+        }
+        let progressc22=document.getElementsByClassName('progressc2')[0];
+        let block22=document.getElementsByClassName('block2');
+    
+        for(var i = 1; i < 100; i ++){
+            progressc22.innerHTML+=`<div class="block2"></div>`
+            block22[i].style.transform="rotate("+3.6 * i +"deg)"
+            block22[i].style.animationDelay=`${i/45}s`;
+    
+        }
+        const circulo2=document.querySelector('.circulo2')
+        let target2 = +circulo2.getAttribute('data-target');
+        let totalPD= data[2].second_stage.payloads.composite_fairing.diameter.meters/100*36.0
+        target2=totalPD;
+        for (let a = 0; a < target2; a++) {
+            block22[a].classList.add('target2')
+            
+        }
+        let progressc33=document.getElementsByClassName('progressc3')[0];
+        let block33=document.getElementsByClassName('block3');
+    
+        for(var i = 1; i < 100; i ++){
+            progressc33.innerHTML+=`<div class="block3"></div>`
+            block33[i].style.transform="rotate("+3.6 * i +"deg)"
+            block33[i].style.animationDelay=`${i/45}s`;
+    
+        }
+        const circulo3=document.querySelector('.circulo3')
+        let target3 = +circulo3.getAttribute('data-target');
+        let totalTw= data[2].engines.thrust_to_weight/100*36.0
+        target3=totalTw;
+        for (let a = 0; a < target3; a++) {
+            block33[a].classList.add('target3')
+            
+        }
+        console.log(target3)
+    })
+}
+
+function progreess3(){
+    fetch("https://api.spacexdata.com/v4/rockets")
+    .then(res=>res.json())
+    .then(data=>{
+        let progressc11=document.getElementsByClassName('progressc1')[0];
+        let blocked=document.getElementsByClassName('block');
+    
+        for(var i = 1; i < 100; i ++){
+            progressc11.innerHTML+=`<div class="block"></div>`
+            blocked[i].style.transform="rotate("+3.6 * i +"deg)"
+            blocked[i].style.animationDelay=`${i/45}s`;
+    
+        }
+        const circulo=document.querySelector('.circulo')
+        let target = +circulo.getAttribute('data-target');
+        let totalPH= 0;
+        target=totalPH;
+        for (let a = 0; a < target; a++) {
+            blocked[a].classList.add('target')
+            
+        }
+        let progressc22=document.getElementsByClassName('progressc2')[0];
+        let block22=document.getElementsByClassName('block2');
+    
+        for(var i = 1; i < 100; i ++){
+            progressc22.innerHTML+=`<div class="block2"></div>`
+            block22[i].style.transform="rotate("+3.6 * i +"deg)"
+            block22[i].style.animationDelay=`${i/45}s`;
+    
+        }
+        const circulo2=document.querySelector('.circulo2')
+        let target2 = +circulo2.getAttribute('data-target');
+        let totalPD=0;
+        target2=totalPD;
+        for (let a = 0; a < target2; a++) {
+            block22[a].classList.add('target2')
+            
+        }
+        let progressc33=document.getElementsByClassName('progressc3')[0];
+        let block33=document.getElementsByClassName('block3');
+    
+        for(var i = 0; i < 100; i ++){
+            progressc33.innerHTML+=`<div class="block3"></div>`
+            block33[i].style.transform="rotate("+3.6 * i +"deg)"
+            block33[i].style.animationDelay=`${i/45}s`;
+    
+        }
+        const circulo3=document.querySelector('.circulo3')
+        let target3 = +circulo3.getAttribute('data-target');
+        let totalTw= data[3].engines.thrust_to_weight/100*36.0
+        target3=totalTw;
+        for (let a = 0; a < target3; a++) {
+            block33[a].classList.add('target3')
+            
+        }
+        console.log(target3)
+    })
 }
 
 
-progreess()
-
-
-
-
-/*
-const circulo=document.querySelector('.circulo')
-const target = +circulo.getAttribute('data-target');
-var elements = document.querySelectorAll("p:nth-child(-n)")
-elements = +target
-document.documentElement.style.setProperty("--variable",elements)
-console.log(target)
-console.log(elements)
-const estilos=getComputedStyle(document.documentElement)
-const valor=estilos.getPropertyValue('--variable')
-console.log(valor)
-*/
 
 
 
